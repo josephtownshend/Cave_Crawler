fc78bf86d2987cb93f07eca6d3cf9743b26104d4
Joseph Townshend <joetownshend@mac.com>
2020-03-29 18:06:53 +0100

    game working with all mechanics

---
+++ src/Game.java
@@ -0,0 +1,90 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * A simple Swing program that creates a window with a button.
+ * The button updates a counter each time it is clicked. There is a label
+ * that displays the counter value.
+ *
+ * @author Joseph Townshend
+ * @version Mar 29, 2020
+ *
+ */
+
+public class Game implements ActionListener{
+
+  private int count = 1;
+  private final JLabel label;
+  private final JFrame frame;
+  private final JButton upButton;
+  private final JButton downButton;
+  private final JPanel panel;
+
+  public Game() {
+
+    frame = new JFrame();
+
+    upButton = new JButton("Up");
+    upButton.addActionListener(this);
+
+    downButton = new JButton("Down");
+    downButton.addActionListener(this);
+
+    label = new JLabel();
+    label.setText("You find yourself in a long passage");
+
+    panel = new JPanel();
+    panel.setBorder(BorderFactory.createEmptyBorder(30, 30 , 10 , 30));
+    panel.setLayout(new GridLayout(2, 1));
+
+    panel.add(upButton);
+    panel.add(downButton);
+    panel.add(label);
+
+    frame.add(panel, BorderLayout.CENTER); // add panel to frame
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // behaviour for closing frame
+    frame.setTitle("Our GUI"); // set title of frame
+    frame.pack(); // set frame to certain size
+    frame.setVisible(true); // set window to be visible and in focus
+
+  }
+
+  public static void main(String[] args) {
+    new Game();
+  }
+
+
+  /**
+   * Increments the counter on each button click and displays
+   * the new value.
+   * @param event information about the button click
+   */
+  @Override
+  public void actionPerformed(ActionEvent event) {
+
+    if (count == 1 && event.getSource() == upButton) {
+      count++;
+      label.setText("You walk into a dark cave");
+    } else if (count == 1 && event.getSource() == downButton) {
+      count--;
+      label.setText("You walk out into sunlight");
+    } else if (count == 2 && event.getSource() == upButton) {
+      label.setText("You can go no further");
+    } else if (count == 2 && event.getSource() == downButton) {
+      count--;
+      label.setText("You find yourself in a long passage");
+    } else {
+      return;
+    }
+
+  }
+}
+
